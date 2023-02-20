2ae7f6ae7e781a784bf927c3a5b484ec381c7132
ilyess-gadecha <98427048+ilyess-gadecha@users.noreply.github.com>
2023-02-20 21:53:08 +0100

    Updates

---
+++ precache-manifest.9792a2ed27e8a490288fde7f17e045a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f7fdfea73f7ace796f4656323fe78d",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "a7432f7e93b0e355d0d0",
+    "url": "/expense-tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "ad80fbfd7b27dad86506",
+    "url": "/expense-tracker/static/js/2.4e4386d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense-tracker/static/js/2.4e4386d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7432f7e93b0e355d0d0",
+    "url": "/expense-tracker/static/js/main.74c6415a.chunk.js"
+  },
+  {
+    "revision": "7284d3afe7f27382a2ae",
+    "url": "/expense-tracker/static/js/runtime-main.ace402c5.js"
+  }
+]);
